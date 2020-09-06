0b1c695fdf7a6f4e1486f245c6b108c3de8425d3
Nathan-Moignard <nathanmoignard@gmail.com>
2020-09-06 01:53:41 +0000

    [ADD] Implementing GIPHY API to send GIF on servers

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const Discord = require ('discord.js')
-const API_KEY = "NzAwODI1MTI3NTI4NDMxNzA2.XpouOA.M5rGpweuJdju-ESevqfDgDNTF0g"
+const Discord = require ('discord.js');
+const jquery = require ('jquery');
+const fetch = require('node-fetch');
+const config = require('./config.json');
+const keys = require ('./keys.json')
 
-const fs = require('fs')
-const logPath = "/home/pi/Simple-Reaction-Bot-Discord-NodeJS/console.log"
-
-const Client = new Discord.Client()
-
-var MICKAEL_ID = "375384599934533642";
-var VALENTIN_ID = "211104990306107392";
-var JMLP_BOT_ID = "472027453477355520";
-var BOT_ID = "700825127528431706";
-
-const AdminRole = "Proviseurs";
+const Client = new Discord.Client();
 
 Client.on('ready', () => {
 
@@ -19,26 +12,16 @@ Client.on('ready', () => {
 });
 
 Client.on('message', message => {
-	// console.log(message.content);
-	// console.log(BOT_ID.indexOf(message.conten
-	console.log("Author: " + message.author.id);
-	if (message.content.toString().indexOf(MICKAEL_ID) > 0 && 0)
-		message.channel.send('miroir miroir :slight_smile:');
-        if (message.content.toString().indexOf(BOT_ID) > 0 &&
-	message.author.id != JMLP_BOT_ID)
-                message.channel.send('Heureux d\'etre parmis vous :wink:');
-	if (message.content.toString().indexOf(BOT_ID) > 0 &&
-        message.author.id == JMLP_BOT_ID)
-		message.react(':rage:')
-			.then(console.log)
-			.catch(console.error);
-	if (message.content.toString().indexOf(VALENTIN_ID) > 0)
-		message.channel.send('Bonne nuit Valou :heart:');
-
-	fs.appendFile(logPath, message.author.username + " send " + message.content + "\n", (error_append) => {
-		if (error_append)
-			throw error_append;
-	});
+	console.log(message.content);
+	if (message.content.startsWith(config.prefix)) {
+		fetch("http://api.giphy.com/v1/gifs/search?q=" + message.content.substring(config.prefix.length) + "&limit=1&api_key=" + keys.API_KEY_GIPHY)
+			.then(res => res.json())
+			.then(json => {
+				console.log("Gif asked: " + message.content.substring(config.prefix.length))
+				message.channel.send(json.data[0].url);
+			});
+		message.delete();
+	}
 });
 
-Client.login(API_KEY)
+Client.login(keys.API_KEY_DISCORD)
