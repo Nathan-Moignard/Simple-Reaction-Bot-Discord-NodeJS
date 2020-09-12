e7e0b32afb162299000b69ce626e36acd2cac1e7
Nathan-Moignard <nathanmoignard@gmail.com>
2020-09-12 23:01:55 +0000

    [UPDATE] Reformating index.js to prepare VoiceManager interations

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require ('discord.js');
 const jquery = require ('jquery');
 const fetch = require('node-fetch');
 const config = require('./config.json');
-const keys = require ('./keys.json')
+const keys = require ('./keys.json');
+const ytdl = require('ytdl-core');
 
 const Client = new Discord.Client();
 
@@ -13,15 +14,25 @@ Client.on('ready', () => {
 
 Client.on('message', message => {
 	console.log(message.content);
-	if (message.content.startsWith(config.prefix)) {
-		fetch("http://api.giphy.com/v1/gifs/search?q=" + message.content.substring(config.prefix.length) + "&limit=1&api_key=" + keys.API_KEY_GIPHY)
-			.then(res => res.json())
-			.then(json => {
-				console.log("Gif asked: " + message.content.substring(config.prefix.length))
-				message.channel.send(json.data[0].url);
-			});
-		message.delete();
+	if (message.content.startsWith(config.prefix + "sound")) {
+		playSound(message);
+	} else if (message.content.startsWith(config.prefix + " ")) {
+		sendGiphy(message);
 	}
 });
+
+function sendGiphy(message) {
+	fetch("http://api.giphy.com/v1/gifs/search?q=" + message.content.substring(config.prefix.length) + "&limit=1&api_key=" + keys.API_KEY_GIPHY)
+	.then(res => res.json())
+	.then(json => {
+		console.log("Gif asked: " + message.content.substring(config.prefix.length));
+		message.channel.send(json.data[0].url);
+	});
+message.delete();
+}
+
+function playSound(message) {
+	message.channel.send("sound command detected");
+}
 
 Client.login(keys.API_KEY_DISCORD)
