7a93eb9178788a2436ca6dc3d6febf7c5b3eb1c8
Nathan-Moignard <nathanmoignard@gmail.com>
2020-09-13 02:12:24 +0000

    [ADD] Adding IBM Watson API Text to Speech support to DiscorsBot

--- index.js
+++ index.js
@@ -1,38 +1,84 @@
-const Discord = require ('discord.js');
-const jquery = require ('jquery');
-const fetch = require('node-fetch');
 const config = require('./config.json');
 const keys = require ('./keys.json');
-const ytdl = require('ytdl-core');
+
+const Discord = require ('discord.js');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const textToSpeech = new TextToSpeechV1({
+	authenticator: new IamAuthenticator({
+		apikey: keys.API_KEY_WATSON_TEXT_TO_SPEECH,
+  	}),
+	serviceUrl: keys.URL_WATSON_TEXT_TO_SPEECH,
+	disableSslVerification: true,
+});
+
+const getVoiceParams = {
+	voice: "fr-FR_NicolasV3Voice",
+};
+
+const synthesizeParams = {
+	text: '',
+	accept: 'audio/wav',
+	voice: 'fr-FR_NicolasV3Voice',
+};
 
 const Client = new Discord.Client();
 
 Client.on('ready', () => {
 
-	console.log("Bot Online")
+	console.log("Bot Online");
 });
 
 Client.on('message', message => {
 	console.log(message.content);
-	if (message.content.startsWith(config.prefix + "sound")) {
+	if (message.content.startsWith(config.sound_prefix)) {
 		playSound(message);
-	} else if (message.content.startsWith(config.prefix + " ")) {
+	} else if (message.content.startsWith(config.yt_prefix)) {
+		
+	} else if (message.content.startsWith(config.gif_prefix)) {
 		sendGiphy(message);
 	}
 });
 
 function sendGiphy(message) {
-	fetch("http://api.giphy.com/v1/gifs/search?q=" + message.content.substring(config.prefix.length) + "&limit=1&api_key=" + keys.API_KEY_GIPHY)
+	fetch("http://api.giphy.com/v1/gifs/search?q=" + message.content.substring(config.gif_prefix.length) + "&limit=1&api_key=" + keys.API_KEY_GIPHY)
 	.then(res => res.json())
 	.then(json => {
-		console.log("Gif asked: " + message.content.substring(config.prefix.length));
+		console.log("Gif asked: " + message.content.substring(config.gif_prefix.length));
 		message.channel.send(json.data[0].url);
 	});
 message.delete();
 }
 
+async function play(voiceChannel) {
+	const connection = await voiceChannel.join();
+	const dispatcher = await connection.play(fs.createReadStream('./temp.wav'));
+
+	dispatcher.on('finish', () => {
+		synthesizeParams.text = "";
+		connection.disconnect();
+	});
+}
+
 function playSound(message) {
+
+	synthesizeParams.text = message.content.substring(config.sound_prefix.length)
 	message.channel.send("sound command detected");
+	textToSpeech.synthesize(synthesizeParams)
+	.then(response => {
+    	return textToSpeech.repairWavHeaderStream(response.result);
+  	})
+	.then(buffer => {
+		fs.writeFileSync('temp.wav', buffer)
+		if (message.member.voice.channel)
+		play(message.member.voice.channel);
+  	})
+  	.catch(err => {
+    	console.log('error:', err);
+	  });
 }
 
 Client.login(keys.API_KEY_DISCORD)
